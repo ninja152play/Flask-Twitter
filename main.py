e98e15903186619fbd64b3b394eb9d29c6354321
ninja <ilyatarasov2006@gmail.com>
2025-06-18 23:37:22 +0300

    Created routes and swagger

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from app.app import create_app
 
+app = create_app()
 
 if __name__ == '__main__':
-    create_app().run(debug=True)
+    dist_path = os.path.join(os.path.dirname(__file__), 'dist')
+    app.run(debug=True, extra_files=[os.path.join(dist_path, 'index.html')])
     #To prod remove debug=True
